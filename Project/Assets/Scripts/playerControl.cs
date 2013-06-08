49e156fb4b26f91c359c2cc7b224dac73fb63e47
Christopher Wade <cjacobwade@gmail.com>
2013-06-07 21:03:41 -0500

    Player movement and sticking to ground

--- Project/Assets/Scripts/playerControl.cs
+++ Project/Assets/Scripts/playerControl.cs
@@ -3,11 +3,20 @@ using System.Collections;
 
 public class playerControl : MonoBehaviour {
 	
-	public int moveSpeed;
-	public int rotateSpeed;
-	
+	//Objects
 	public GameObject cube;
 	public Camera camera1;
+	
+	//Character Control
+		public int moveSpeed;
+		public int rotateSpeed;
+		public Vector3 moveDirection;
+	
+		//Jumping
+		public int jumpSpeed;
+		public int gravity;
+		private bool isGrounded;
+
 	
 	// Use this for initialization
 	void Start () {
@@ -15,35 +24,55 @@ public class playerControl : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 	
 		PlayerControl();
-
+		if (isGrounded) 
+			IsGrounded();
+		else
+			moveDirection.y -= gravity * Time.deltaTime;
+		transform.Translate(moveDirection*Time.deltaTime);
 	}
 	
-	void PlayerControl()
+	void IsGrounded() //When on the ground
 	{
+		print("Is Grounded");
+        moveDirection = new Vector3(0, 0, 0);
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= moveSpeed;
+        if (Input.GetKey(KeyCode.Space))
+            moveDirection.y = jumpSpeed;
+	}
 	
+	void OnTriggerEnter(Collider other)//On collision with stuff
+	{
+		if(other.gameObject.tag == "Ground")//if the ground
+			isGrounded = true;
+	}
+	
+	void OnTriggerExit(Collider other)//If leaving collision with stuff
+	{
+		if(other.gameObject.tag == "Ground")
+			isGrounded = false;
+	}	
+	
+	void PlayerControl()//Control Player
+	{
 		if(Input.GetKey(KeyCode.W))
 		{
 			transform.Translate(new Vector3(moveSpeed,0,0)*Time.deltaTime);
 		}
-			
 		if(Input.GetKey(KeyCode.S))
 		{
 			transform.Translate(new Vector3(-moveSpeed,0,0)*Time.deltaTime);
-		}
-				
+		}	
 		if(Input.GetKey(KeyCode.A))
 		{
 			transform.Translate(new Vector3(0,0,moveSpeed)*Time.deltaTime);
-		}
-					
+		}			
 		if(Input.GetKey(KeyCode.D))
 		{
 			transform.Translate(new Vector3(0,0,-moveSpeed)*Time.deltaTime);
 		}
-	}
-		
-		
+	}	
 }

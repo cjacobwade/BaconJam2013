acfbe372b18f1fdcbd87ead02c1d14fde4a4c5e6
Christopher Wade <cjacobwade@gmail.com>
2013-06-07 19:54:02 -0500

    revised player controls

--- Project/Assets/Scripts/playerControl.cs
+++ Project/Assets/Scripts/playerControl.cs
@@ -17,29 +17,33 @@ public class playerControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		PlayerControl();
+
+	}
+	
+	void PlayerControl()
+	{
+	
 		if(Input.GetKey(KeyCode.W))
 		{
 			transform.Translate(new Vector3(moveSpeed,0,0)*Time.deltaTime);
-			
-			
 		}
 			
 		if(Input.GetKey(KeyCode.S))
 		{
 			transform.Translate(new Vector3(-moveSpeed,0,0)*Time.deltaTime);
-			
 		}
 				
 		if(Input.GetKey(KeyCode.A))
 		{
 			transform.Translate(new Vector3(0,0,moveSpeed)*Time.deltaTime);
-			//transform.Rotate(Vector3.up,-rotateSpeed*Time.deltaTime); //Rotate character
 		}
 					
 		if(Input.GetKey(KeyCode.D))
 		{
 			transform.Translate(new Vector3(0,0,-moveSpeed)*Time.deltaTime);
-			//transform.Rotate(Vector3.up,rotateSpeed*Time.deltaTime);
 		}
 	}
+		
+		
 }
